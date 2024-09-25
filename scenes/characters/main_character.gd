10c1e5825b81cfca7967564c6e4aeb804c2f5cd7
suisarbre <106571710+suisarbre@users.noreply.github.com>
2024-09-25 12:42:47 -0700

    movement implemented!

--- scenes/characters/main_character.gd
+++ scenes/characters/main_character.gd
@@ -5,7 +5,20 @@ const WALKSPEED = 5.0
 const RUNSPEED = 10.0
 const JUMP_VELOCITY = 4.5
 
+@onready var head := $Head
+@onready var camera := $Head/Camera3D
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			head.rotate_y(-event.relative.x * 0.01)
+			camera.rotate_x(-event.relative.y * 0.01)
+			camera.rotation.x = clamp(camera.rotation.x,deg_to_rad(-30),deg_to_rad(60))
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -29,7 +42,7 @@ func _physics_process(delta: float) -> void:
 		Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
 	)
 	
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
